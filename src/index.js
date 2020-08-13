e799283169a4c41c3c33021e3b8c939e9d566be7
Andrew Smith <smith.f.andrew@gmail.com>
2020-08-13 17:08:49 -0300

    added texture for testing

--- src/index.js
+++ src/index.js
@@ -20,9 +20,7 @@ const program = createShaderProgram(vsSource, fsSource);
 const a_position = gl.getAttribLocation(program, "a_position");
 
 // Create a new Vertext Buffer of float 32 to hold each vertex
-const vb = new VertexBuffer(
-  new Float32Array([-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5])
-);
+const vb = new VertexBuffer(new Float32Array([-0.5, -0.5, 0.5, -0.5, 0.5, 0.5, -0.5, 0.5]));
 // Create a new Index buffer that points to vertex indecies required to build a square
 const ib = new IndexBuffer(new Int16Array([0, 1, 2, 2, 3, 0]));
 
@@ -37,3 +35,24 @@ gl.vertexAttribPointer(a_position, 2, gl.FLOAT, false, 0, 0);
 
 // Tell it to draw based on the index buffer
 gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+
+// fetch("/image.dds")
+//   .then((r) => r.arrayBuffer())
+//   .then((data) => {
+//     const image = new DataView(data);
+//     const ext =
+//       gl.getExtension("WEBGL_compressed_texture_s3tc") ||
+//       gl.getExtension("MOZ_WEBGL_compressed_texture_s3tc") ||
+//       gl.getExtension("WEBKIT_WEBGL_compressed_texture_s3tc");
+//     const texture = gl.createTexture();
+//     gl.bindTexture(gl.TEXTURE_2D, texture);
+//     gl.compressedTexImage2D(gl.TEXTURE_2D, 0, ext.COMPRESSED_RGBA_S3TC_DXT5_EXT, 512, 512, 0, image);
+
+//     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+//     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+//     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+//     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+//     // Tell it to draw based on the index buffer
+//     gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0);
+//   });
